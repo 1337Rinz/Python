f01972f8529a134b2a71f47054c25e153157b81b
RinzCSGO <71659426+RinzCSGO@users.noreply.github.com>
2022-11-08 23:54:49 +0700

    Create protonX02.py

---
+++ protonX02.py
@@ -0,0 +1,42 @@
+def exercise2(xs, y):
+    
+
+    result = 0
+    y = y.split()
+
+
+    for i in xs:
+      if i in y:
+        result += 1
+
+    return result
+    
+    
+# TEST CODE
+SECOND = '2'
+try:
+    q = SECOND
+    inp = [
+      (['hổ', 'bò', 'gà', 'tủ lạnh'], 'hổ mang bò lên núi'), 
+      (['hổ', 'bò', 'gà', 'tủ lạnh'], 'Hổ mang bò lên núi'), 
+      ([['1', '2', '1111', '1'], '11111111']),
+    ]
+    out = [
+    2,
+    1,
+    3,
+    ]
+    
+    res_list = [False for _ in range(len(inp))]
+    for i, inpi in enumerate(inp):
+        res_list[i] = exercise2(*inpi) == out[i]
+    
+    ans = 'Câu {}:\n'.format(q)
+    for i, item in enumerate(res_list):
+        if not res_list[i]:
+            ans += "\nTest case {} được tính đúng?: {}.\n\t Giá trị đầu vào: {}".format(i + 1, res_list[i], inp[i])
+        else:
+            ans += "\nTest case {} được tính đúng?: {}.".format(i + 1, res_list[i])
+    print(ans)
+except Exception as e:
+    print("Lỗi thực thi: ", e)
